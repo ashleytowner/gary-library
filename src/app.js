a6fedfc59702d5d8363cc7d281e92652fdf91124
Ashley Towner <git@ashtowner.com>
2025-01-02 17:57:15 +1030

    Improved everything

--- src/app.js
+++ src/app.js
@@ -10,37 +10,51 @@ const app = express();
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS Items (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT,
+		name TEXT NOT NULL,
 		description TEXT,
-		format TEXT,
-		owner INTEGER,
+		format TEXT NOT NULL,
+		owner INTEGER NOT NULL,
 		FOREIGN KEY (owner) REFERENCES Users(id)
 	)`);
   db.run(`CREATE TABLE IF NOT EXISTS Users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT,
-		password TEXT,
+		username TEXT NOT NULL,
+		password TEXT NOT NULL,
 		is_admin INTEGER
-	)`);
-  db.run(`CREATE TABLE IF NOT EXISTS Movements (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		item INTEGER,
-		origin INTEGER,
-		destination INTEGER,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		FOREIGN KEY (item) REFERENCES Items(id),
-		FOREIGN KEY (origin) REFERENCES Users(id),
-		FOREIGN KEY (destination) REFERENCES Users(id)
 	)`);
   db.run(`CREATE TABLE IF NOT EXISTS Sessions (
 		id TEXT PRIMARY KEY,
-		user INTEGER,
+		user INTEGER NOT NULL,
 		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (user) REFERENCES Users(id)
+	)`);
+  db.run(`CREATE TABLE IF NOT EXISTS Requests (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		action TEXT NOT NULL CHECK (action IN ('borrow', 'consult')),
+		item INTEGER NOT NULL,
+		user INTEGER NOT NULL,
+		status TEXT NOT NULL CHECK (status IN ('pending', 'rejected', 'approved')),
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (item) REFERENCES Items(id),
+		FOREIGN KEY (user) REFERENCES Users(id)
+	)`);
+  db.run(`CREATE TABLE IF NOT EXISTS Borrows (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		action TEXT NOT NULL CHECK (action IN ('borrow', 'return')),
+		item INTEGER NOT NULL,
+		user INTEGER NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		FOREIGN KEY (item) REFERENCES Items(id),
 		FOREIGN KEY (user) REFERENCES Users(id)
 	)`);
   db.run(`CREATE VIEW IF NOT EXISTS items_view AS
 		SELECT i.id as id, name, description, format, username as owner FROM Items i
 		JOIN Users u ON i.owner = u.id`);
+  db.run(`CREATE VIEW IF NOT EXISTS ledger AS
+		SELECT l.id as id, i.name as item, i.id as item_id, u.username as user, l.status, l.is_borrow, l.created_at FROM Lends l
+		JOIN Users u ON l.user = u.id
+		JOIN Items i ON l.item = i.id
+	`);
   const pword = bcrypt.hashSync("admin", 10);
   db.run(
     "INSERT OR IGNORE INTO Users (id, username, password, is_admin) VALUES (1, ?, ?, 1)",
@@ -54,6 +68,12 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", "src/views");
 app.use(express.static("src/public"));
+
+app.use((req, res, next) => {
+  res.locals.loggedIn = false;
+  res.locals.isAdmin = false;
+  next();
+});
 
 app.get("/logout", (req, res) => {
   res.clearCookie("Authorization");
@@ -139,8 +159,9 @@ app.use((req, res, next) => {
           return;
         }
 
-        req.userId = rows[0].user;
-        req.isAdmin = Boolean(rows[0].is_admin);
+        res.locals.userId = rows[0].user;
+        res.locals.isAdmin = Boolean(rows[0].is_admin);
+        res.locals.loggedIn = true;
 
         next();
       },
@@ -148,21 +169,32 @@ app.use((req, res, next) => {
   }
 });
 
-app.use((req, res, next) => {
+app.use((req, _res, next) => {
   req.hxRequest = req.headers["hx-request"] === "true";
   next();
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
+  const links = [
+    '<a href="/items">View Items</a>',
+    '<a href="/items/create">Add an Item</a>',
+  ];
+  if (res.locals.isAdmin) {
+    links.push(
+      ...[
+        '<a href="/users">View Users</a>',
+        '<a href="/users/create">Add a user</a>',
+      ],
+    );
+  }
   res.render("layout", {
     title: "Home",
-    body: '<h1>Welcome to the Gary Library</h1><p hx-get="/items" hx-trigger="load"><a href="/items">View Items</a>',
+    body: `<h1>Welcome to the Gary Library</h1><p>${links.join(" • ")}</p>`,
   });
 });
 
 app.get("/items", (req, res) => {
   const { search } = req.query;
-  console.log("###", search);
   const query = `SELECT * FROM items_view WHERE ${
     search ? "name LIKE ?" : "1=1"
   }`;
@@ -199,16 +231,19 @@ app.get("/items", (req, res) => {
       </form>`;
       req.hxRequest
         ? res.send(html)
-        : res.render("layout", { title: "Items", body: searchForm + html });
+        : res.render("layout", {
+            title: "Items",
+            body: '<a href="/items/create">Add Item</a>' + searchForm + html,
+          });
     }
   });
 });
 
-app.get("/items/create", (req, res) => {
+app.get("/items/create", (_req, res) => {
   res.render("layout", {
     title: "Create Item",
     body: `
-    <form hx-post="/items">
+    <form hx-boost="true" method="POST" action="/items">
       <label for="name">Name</label>
       <input type="text" name="name" />
       <label for="description">Description</label>
@@ -232,20 +267,202 @@ app.get("/items/create", (req, res) => {
 });
 
 app.get("/items/:id", (req, res) => {
-  db.all(
-    "SELECT * FROM items_view WHERE id = ?",
+  db.all("SELECT * FROM items WHERE id = ?", req.params.id, (err, rows) => {
+    if (err) {
+      console.error("Could not fetch item", err);
+      return;
+    }
+    if (rows.length === 0) {
+      res.sendStatus(404);
+      return;
+    }
+    const item = rows[0];
+    console.log(item);
+    let html = `<div>
+				<h1>${item.name}</h1>
+				<p>${item.description}</p>
+				<button hx-post="/items/${item.id}/request">Request to Borrow</button>
+			</div>`;
+    db.all(
+      'SELECT r.*, u.username FROM Requests r JOIN Users u ON r.user = u.id WHERE item = ? AND status = "pending" ORDER BY created_at DESC',
+      item.id,
+      (err, rows) => {
+        if (err) {
+          console.error("Could not fetch requests for item", err);
+        } else if (rows.length > 0) {
+          const isOwner = item.owner === res.locals.userId;
+          html += `<h2>Requests</h2>
+            <table>
+              <tr>
+                <th>User</th>
+                <th>Date</th>
+                ${isOwner ? "<th>Actions</th>" : ""}
+              </tr>
+              ${rows
+                .map(
+                  (row) =>
+                    `<tr id="request-${row.id}">
+                    <td>${row.username}</td>
+                    <td>${row.created_at}</td>
+                    ${
+                      isOwner
+                        ? `<td>
+                            <button hx-target="#request-${row.id}" hx-put="/requests/${row.id}/approve">
+                              Approve</button
+                            ><button hx-target="#request-${row.id}" hx-put="/requests/${row.id}/reject">
+                              Reject
+                            </button>
+                          </td>`
+                        : ""
+                    }
+                  </tr>`,
+                )
+                .join("")}
+            </table>`;
+        }
+        res.render("layout", { title: item.name, body: html });
+      },
+    );
+  });
+});
+
+app.post("/items/:id/request", (req, res) => {
+  let { action } = req.body;
+  if (!action) {
+    // res
+    //   .status(400)
+    //   .send("You must specify an action, either Borrow or Consult");
+    // return;
+    action = "borrow";
+  }
+  db.get("SELECT * FROM items WHERE id = ?", req.params.id, (err, item) => {
+    if (err) {
+      console.error("Cannot fetch item", err);
+      res.sendStatus(500);
+      return;
+    }
+    if (!item) {
+      res.sendStatus(404);
+      return;
+    }
+    if (item.owner === res.locals.userId) {
+      res.status(400).send("You cannot request your own item!");
+      return;
+    }
+    db.get(
+      "SELECT * FROM Requests WHERE user = ? ORDER BY created_at DESC",
+      res.locals.userId,
+      (err, request) => {
+        if (err) {
+          console.error("Could not fetch requests", err);
+          res.sendStatus(500);
+          return;
+        }
+        if (request && request.status === "pending") {
+          res
+            .status(400)
+            .send("You already have a pending request for this item");
+          return;
+        }
+        db.run(
+          'INSERT INTO Requests (action, item, user, status) VALUES (?, ?, ?, "pending")',
+          action,
+          req.params.id,
+          res.locals.userId,
+          (err) => {
+            if (err) {
+              console.error("Could not create a request!", err);
+              res.sendStatus(500);
+              return;
+            }
+            res.setHeader("HX-Refresh", "true");
+            res.sendStatus(201);
+          },
+        );
+      },
+    );
+  });
+});
+
+app.put("/requests/:id/approve", (req, res) => {
+  db.get(
+    "SELECT r.*, i.owner FROM Requests r JOIN Items i ON r.item = i.id WHERE r.id = ?",
     req.params.id,
-    (err, rows) => {
+    (err, row) => {
       if (err) {
-        console.error("Could not fetch item", err);
+        console.error("Could not get request", err);
+        res.sendStatus(500);
         return;
       }
-      if (rows.length === 0) {
+      if (!row) {
         res.sendStatus(404);
         return;
       }
-      const item = rows[0];
-      res.send(`<div><h1>${item.name}</h1><p>${item.description}</p></div>`);
+      if (row.owner !== res.locals.userId) {
+        res.sendStatus(403);
+        return;
+      }
+      // TODO: Add a check to see if it is currently lent out, as you can't approve
+      // a borrow if it is already on loan.
+      db.serialize(() => {
+        try {
+          db.run("BEGIN TRANSACTION");
+
+          db.run(
+            'UPDATE Requests SET status = "approved" WHERE id = ?',
+            req.params.id,
+          );
+
+          db.run(
+            "INSERT INTO Borrows (action, item, user) VALUES (?, ?, ?)",
+            "borrow",
+            row.item,
+            row.user,
+          );
+
+          db.run("COMMIT");
+
+          res.status(201).send("Successfully Approved");
+        } catch (err) {
+          console.error("Could not approve the borrow", err);
+          db.run("ROLLBACK");
+          res.sendStatus(500);
+        }
+      });
+    },
+  );
+});
+
+app.put("/requests/:id/reject", (req, res) => {
+  db.get(
+    "SELECT r.*, i.owner FROM Requests r JOIN Items i ON r.item = i.id WHERE r.id = ?",
+    req.params.id,
+    (err, row) => {
+      if (err) {
+        console.error("Could not get request", err);
+        res.sendStatus(500);
+        return;
+      }
+      if (!row) {
+        res.sendStatus(404);
+        return;
+      }
+      if (row.owner !== res.locals.userId) {
+        res.sendStatus(403);
+        return;
+      }
+      db.run(
+        'UPDATE Requests SET status = "rejected" WHERE id = ?',
+        req.params.id,
+        (err) => {
+          if (err) {
+            console.error("Could not reject request");
+            res.sendStatus(500);
+            return;
+          }
+          res.status(201).send("Successfully Rejected");
+        },
+      );
     },
   );
 });
@@ -257,12 +474,13 @@ app.post("/items", (req, res) => {
     name,
     description,
     format,
-    req.userId,
+    res.locals.userId,
     (err) => {
       if (err) {
         console.error("Failed to add Item", err);
         res.sendStatus(500);
       } else {
+        res.setHeader("HX-Redirect", "/items");
         res.sendStatus(201);
       }
     },
@@ -270,13 +488,36 @@ app.post("/items", (req, res) => {
 });
 
 /** @type {import("express").RequestHandler} */
-const isAdmin = (req, res, next) => {
-  if (!req.isAdmin) {
+const isAdmin = (_req, res, next) => {
+  if (!res.locals.isAdmin) {
     res.sendStatus(403);
   } else {
     next();
   }
 };
+
+app.get("/profile", (_req, res) => {
+  db.get("SELECT * FROM Users WHERE id = ?", res.locals.userId, (err, row) => {
+    if (err) {
+      console.error("Could not fetch user", err);
+      res.sendStatus(500);
+      return;
+    }
+    if (!row) {
+      res.sendStatus(404);
+      return;
+    }
+    res.render("layout", {
+      title: "Profile",
+      body: `<h1>Hello, ${row.username}</h1>
+        <form hx-put="/users/update-password">
+          <label for="new_password">New Password</label>
+          <input type="password" name="new_password" />
+          <button type="submit">Change Password</button>
+        </form>`,
+    });
+  });
+});
 
 app.get("/users", isAdmin, (req, res) => {
   db.all("SELECT * FROM Users", (err, rows) => {
@@ -285,15 +526,19 @@ app.get("/users", isAdmin, (req, res) => {
       res.sendStatus(500);
       return;
     }
-    const table = `<table><tr><th>Username</th><th>Is Admin</th><th></th></tr>${rows.map(
-      (user) =>
-        `<tr><td>${user.username}</td><td>${user.is_admin}</td><td><button hx-delete="/users/${user.id}">Delete</button</td></tr>`,
-    ).join('')}</table>`;
-		req.hxRequest ? res.send(table) : res.render('layout', { title: 'Users', body: table });
+    const table = `<table><tr><th>Username</th><th>Is Admin</th><th></th></tr>${rows
+      .map(
+        (user) =>
+          `<tr><td>${user.username}</td><td>${user.is_admin}</td><td><button hx-delete="/users/${user.id}">Delete</button</td></tr>`,
+      )
+      .join("")}</table>`;
+    req.hxRequest
+      ? res.send(table)
+      : res.render("layout", { title: "Users", body: table });
   });
 });
 
-app.get("/users/create", isAdmin, (req, res) => {
+app.get("/users/create", isAdmin, (_req, res) => {
   res.render("layout", {
     title: "Create User",
     body: `
@@ -326,10 +571,23 @@ app.post("/users", isAdmin, (req, res) => {
   );
 });
 
-app.delete('/users/:id', isAdmin, (req, res) => {
-	db.run('DELETE FROM Users WHERE id = ?', req.params.id);
-	res.setHeader('HX-Redirect', '/users');
-	res.sendStatus(201);
+app.put("/users/update-password", (req, res) => {
+  const { new_password } = req.body;
+  if (!new_password) {
+    res.sendStatus(400);
+    return;
+  }
+  db.run(
+    "UPDATE Users SET password=? WHERE id = ?",
+    bcrypt.hashSync(new_password, 10),
+    res.locals.userId,
+  );
+});
+
+app.delete("/users/:id", isAdmin, (req, res) => {
+  db.run("DELETE FROM Users WHERE id = ?", req.params.id);
+  res.setHeader("HX-Redirect", "/users");
+  res.sendStatus(201);
 });
 
 app.listen(3000, () => {
